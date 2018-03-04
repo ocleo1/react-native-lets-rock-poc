23decf77a2afa3a8867a45b148b9dc7384f78135
Chris Wan <Chris@Wans-MacBook-Pro.local>
2018-03-03 23:41:24 -0500

    Add iOS entry

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+/**
+ * Lets Rock
+ * https://github.com/ocleo1
+ * @flow
+ */
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+export default class LetsRock extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+AppRegistry.registerComponent('LetsRock', () => LetsRock);
