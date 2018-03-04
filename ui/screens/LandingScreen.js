6f3a63a49dc622f4e2298dbb12a495f40779a2b2
Chris Wan <Chris@Wans-MacBook-Pro.local>
2018-03-04 01:54:18 -0500

    Adjust LandingScreen

--- ui/screens/LandingScreen.js
+++ ui/screens/LandingScreen.js
@@ -18,10 +18,7 @@ import {
 
 export default class LandingScreen extends React.Component {
   static navigationOptions = {
-    title: 'Grow Together',
-    headerStyle: {
-      backgroundColor: '#F5FCFF'
-    },
+    header: null
   };
 
   constructor(props) {
@@ -37,7 +34,7 @@ export default class LandingScreen extends React.Component {
     const { height, width } = event.nativeEvent.layout;
     this.setState({
       imageWidth: width,
-      imageHeight: height * 0.6,
+      imageHeight: height * 0.4,
     })
   }
 
@@ -52,24 +49,29 @@ export default class LandingScreen extends React.Component {
 
     return (
       <View style={styles.container} onLayout={this._onLayout.bind(this)}>
-        <View style={{flex: .6}}>
+        <View style={{flex: .2, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={styles.headerText}>Grow Together!</Text>
+        </View>
+        <View style={{flex: .4}}>
           <Image
             style={{width: imageWidth, height: imageHeight}}
             source={require('../images/biking.png')}
           />
         </View>
-        <View style={{flex: .4, backgroundColor: '#c6f9ff', alignItems: 'center', justifyContent: 'center'}}>
-          <TouchableOpacity onPress={this._onPressButton.bind(this)}>
-            <View style={styles.button}>
-              <Text style={styles.buttonText}>
-                Get Started
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <View style={{paddingHorizontal: 10}}>
-            <Text>
+        <View style={{flex: .4, backgroundColor: '#c6f9ff'}}>
+          <View style={{flex: .9, alignItems: 'center', justifyContent: 'center'}}>
+            <TouchableOpacity onPress={this._onPressButton.bind(this)}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>
+                  Get Started
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={{flex: .1, paddingHorizontal: 10}}>
+            <Text style={{fontSize: 10, textAlign: 'center'}}>
               By continuing, you agree to our
-              <Text style={{fontSize: 10, fontWeight: 'bold'}}> Terms of Use & Privacy Policy</Text>
+              <Text style={{fontWeight: 'bold'}}> Terms of Use & Privacy Policy</Text>
             </Text>
           </View>
         </View>
@@ -83,12 +85,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
+  headerText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
   button: {
-    width: 200,
+    width: 300,
     height: 30,
     backgroundColor: '#2dd2e5',
     borderColor: '#2dd2e5',
-    borderRadius: 10,
+    borderRadius: 20,
     borderWidth: 1,
     alignItems: 'center',
     justifyContent: 'center'
